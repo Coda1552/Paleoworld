f2769f4b122c44e7fa6c3c9d725dc10bd49c8918
Coda <cody.koelliker@outlook.com>
2021-10-26 16:01:08 -0600

    Cephalaspis variant

--- src/main/java/coda/paleoworld/client/renderer/CephalaspisRenderer.java
+++ src/main/java/coda/paleoworld/client/renderer/CephalaspisRenderer.java
@@ -4,26 +4,33 @@ import coda.paleoworld.Paleoworld;
 import coda.paleoworld.client.model.CephalaspisModel;
 import coda.paleoworld.common.entities.CephalaspisEntity;
 import coda.paleoworld.common.entities.CephalaspisEntity;
+import com.google.common.collect.Maps;
 import com.mojang.blaze3d.matrix.MatrixStack;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
 import net.minecraft.client.renderer.entity.MobRenderer;
 import net.minecraft.entity.passive.fish.CodEntity;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.Util;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.vector.Vector3f;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
+import java.util.Map;
+
 @OnlyIn(Dist.CLIENT)
 public class CephalaspisRenderer extends MobRenderer<CephalaspisEntity, CephalaspisModel<CephalaspisEntity>> {
-   private static final ResourceLocation TEXTURE = new ResourceLocation(Paleoworld.MOD_ID, "textures/entity/fish/cephalaspis.png");
+   public static final Map<Integer, ResourceLocation> TEXTURES = Util.make(Maps.newHashMap(), (hashMap) -> {
+      hashMap.put(0, new ResourceLocation(Paleoworld.MOD_ID, "textures/entity/fish/cephalaspis_green.png"));
+      hashMap.put(1, new ResourceLocation(Paleoworld.MOD_ID, "textures/entity/fish/cephalaspis_yellow.png"));
+   });
 
    public CephalaspisRenderer(EntityRendererManager renderManagerIn) {
       super(renderManagerIn, new CephalaspisModel<>(), 0.3F);
    }
 
    public ResourceLocation getTextureLocation(CephalaspisEntity entity) {
-      return TEXTURE;
+      return TEXTURES.getOrDefault(entity.getVariant(), TEXTURES.get(0));
    }
 
    protected void setupRotations(CephalaspisEntity p_225621_1_, MatrixStack p_225621_2_, float p_225621_3_, float p_225621_4_, float p_225621_5_) {
